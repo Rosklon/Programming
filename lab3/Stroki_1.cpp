8b42601173a46e20d5a2fef41b3fb6db59f04236
Rosklon <57514687+Rosklon@users.noreply.github.com>
2020-06-05 18:31:18 +0700

    Create Stroki_1.cpp

---
+++ lab3/Stroki_1.cpp
@@ -0,0 +1,41 @@
+﻿
+#include <iostream> 
+
+using namespace std;
+
+void LowerString(char* s);
+
+int main()
+{
+    setlocale(LC_ALL, "ru");
+    char* s = new char[1];
+    cout << "Введите предложение на английском языке: " << endl;
+    cin.getline(s, 80);
+    cout << "Измененное предложение: " << endl;
+    LowerString(s);
+}
+
+void LowerString(char* s)
+{
+    int n = strlen(s);
+    int i = 0;
+    for (i = 0; i < n; i++)
+    {
+        if (isalpha(s[i]))
+        {
+            if (s[i] >= 'A' && s[i] <= 'Z')
+            {
+                s[i] += 'a' - 'A';
+                cout << s[i];
+            }
+            else
+                cout << s[i];
+        }
+        else
+        {
+            cout << s[i];
+        }
+
+    }
+}
+
